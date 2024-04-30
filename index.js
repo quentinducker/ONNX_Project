116a9ebe849176c7db3221ff4a718f12ef727ad6
quentinducker <99065473+quentinducker@users.noreply.github.com>
2024-04-30 18:03:34 -0500

    Update index.js

--- index.js
+++ index.js
@@ -36,7 +36,7 @@ async function runExample() {
    <table>
      <tr>
        <td>  Concrete Result:  </td>
-       <td id="td0">  ${outputData.data[0].toFixed(2)}  </td> //may have to change this
+       <td id="td0">  ${outputData.toFixed(2)}  </td> //may have to change this
      </tr>
   </table>`;
     
